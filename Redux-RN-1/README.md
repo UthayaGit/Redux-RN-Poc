6c0f7d3b9ec21b61970895ca53d08936554fa346
Uthaya Chandiran <Uthaya.Chandiran@damanhealth.ae>
2019-12-29 17:05:26 +0400

    commit on 'master'

---
+++ Redux-RN-1/README.md
@@ -0,0 +1,35 @@
+# React Native Redux
+
+1. Redux integation in React Native.
+2. Webservice integration using axios.
+3. FlatList integration in React Native 
+
+
+## Clone Repository
+
+```bash
+git clone https://github.com/tekhubapps/Redux-FlatList.git
+```
+
+## Install node modules
+
+```bash
+cd Redux-FlatList
+```
+```bash
+npm install
+```
+
+## Run Application
+
+```bash
+npm start
+```
+Open new Terminal
+```bash
+react-native run-ios
+``` 
+Open new Terminal
+```bash
+react-native run-android
+``` 
---
+++ Redux-RN-1/app/reducers/UserReducer.js
@@ -0,0 +1,27 @@
+'use strict';
+
+import {ACTIONS} from '../util/Constants';
+
+let initialState = { 
+  userInfo : {
+    first_name:"Uthaya",
+    last_name:"Chandiran",
+  }, //Conatins user data like id, first_name, last_name, email and avator
+};
+
+const {
+  UPDATE_USER_DATA,
+} = ACTIONS;
+
+export const user = (state = initialState, action) => {
+  const { 
+    type,
+    userInfo,
+  } = action;
+  switch (type) {
+    case UPDATE_USER_DATA:
+      return { ...state, userInfo };
+    default:
+      return state;
+  }
+};
